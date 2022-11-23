1149490e95c6c0e503b4800cd6c0f9761fd8e609
Aleksandra Petrova <petrowa.alex@yandex.com>
2022-11-23 15:16:14 +0300

    added started version

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'color_schemes.g.dart';
+import 'input_page.dart';
+
+void main() => runApp(const BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
+      darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+      home: const InputPage(),
+    );
+  }
+}
