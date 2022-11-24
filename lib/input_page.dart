f127dfec8e1485e8085b61ea6ef9112e51aa50c0
Aleksandra Petrova <petrowa.alex@yandex.com>
2022-11-24 13:09:39 +0300

    Created CardContent widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHight = 80.0;
+const bottomContainerColor = Color.fromARGB(255, 12, 75, 184);
+const primaryColor = Color.fromARGB(255, 186, 171, 171);
+const accentColor = Color.fromARGB(255, 44, 71, 117);
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
@@ -26,15 +30,25 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: ReusableCard(
-                    cardborder: Border.all(
-                      color: Colors.grey,
+                    cardBorder: Border.all(
+                      color: primaryColor,
+                    ),
+                    cardChild: const CardContent(
+                      label: 'мужчина',
+                      icon: Icons.male,
+                      color: Colors.orange,
                     ),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
-                    cardborder: Border.all(
-                      color: Colors.grey,
+                    cardBorder: Border.all(
+                      color: primaryColor,
+                    ),
+                    cardChild: const CardContent(
+                      label: 'женщина',
+                      icon: Icons.female,
+                      color: Color.fromARGB(255, 243, 127, 166),
                     ),
                   ),
                 ),
@@ -43,9 +57,10 @@ class _InputPageState extends State<InputPage> {
           ),
           Expanded(
             child: ReusableCard(
-              cardborder: Border.all(
-                color: Colors.grey,
+              cardBorder: Border.all(
+                color: primaryColor,
               ),
+              cardChild: const Text("TEST"),
             ),
           ),
           Expanded(
@@ -53,24 +68,26 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: ReusableCard(
-                    cardborder: Border.all(
-                      color: Colors.blueGrey,
+                    cardBorder: Border.all(
+                      color: accentColor,
                       width: 3.0,
                     ),
+                    cardChild: const Text("TEST"),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
-                    cardborder: Border.all(
-                      color: Colors.grey,
+                    cardBorder: Border.all(
+                      color: primaryColor,
                     ),
+                    cardChild: const Text("TEST"),
                   ),
                 ),
               ],
             ),
           ),
           Container(
-            color: Colors.blueAccent,
+            color: bottomContainerColor,
             margin: const EdgeInsets.only(
               top: 10.0,
             ),
@@ -83,18 +100,58 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class CardContent extends StatelessWidget {
+  const CardContent(
+      {super.key, required this.label, required this.icon, this.color});
+
+  final String label;
+  final IconData icon;
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          color: color,
+          icon,
+          size: 80.0,
+        ),
+        const SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: const TextStyle(
+            fontSize: 15.0,
+            color: accentColor,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
-  final Border cardborder;
-  const ReusableCard({super.key, required this.cardborder});
+  final Border cardBorder;
+  final Widget? cardChild;
+  const ReusableCard({
+    super.key,
+    required this.cardBorder,
+    this.cardChild,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        border: cardborder,
-        borderRadius: BorderRadius.circular(15.0),
+        border: cardBorder,
+        borderRadius: BorderRadius.circular(5.0),
       ),
+      padding: const EdgeInsets.all(25.0),
       margin: const EdgeInsets.all(10.0),
+      child: cardChild,
     );
   }
 }
