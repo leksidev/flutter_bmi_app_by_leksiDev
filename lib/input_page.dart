81b0b5e992e1d62bdd962de5e765d5176bb6b1f4
Aleksandra Petrova <petrowa.alex@yandex.com>
2022-11-24 10:21:00 +0300

    Made interface frame

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class InputPage extends StatefulWidget {
@@ -17,13 +16,85 @@ class _InputPageState extends State<InputPage> {
           'BMI CALCULATOR',
         ),
       ),
-      body: const Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(15.0)),
+                    margin: const EdgeInsets.all(15.0),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(15.0)),
+                    margin: const EdgeInsets.all(15.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(15.0)),
+              margin: const EdgeInsets.all(15.0),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(15.0)),
+                    margin: const EdgeInsets.all(15.0),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(15.0)),
+                    margin: const EdgeInsets.all(15.0),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {},
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.graphic_eq),
+            label: "First",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.man),
+            label: "Second",
+          ),
+        ],
+      ),
+
+      // floatingActionButton: FloatingActionButton(
+      //   child: const Icon(Icons.add),
+      //   onPressed: () {},
+      // ),
     );
   }
 }
