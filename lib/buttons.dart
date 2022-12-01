adf64a20771ca016d821b9e9dccdd0606cc0c8cd
Aleksandra Petrova <petrowa.alex@yandex.com>
2022-12-01 19:24:26 +0300

    Create separate file buttons with buttons classes inside

---
+++ lib/buttons.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+class BottomButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final String buttonTitle;
+
+  const BottomButton(
+      {required this.buttonTitle, required this.onTap, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        color: kDarkBlueAccentColor,
+        margin: const EdgeInsets.only(
+          top: 10.0,
+        ),
+        padding: const EdgeInsets.only(bottom: 15.0),
+        width: double.infinity,
+        height: kBottomContainerHight,
+        child: const Center(
+          child: Text(
+            'РАССЧИТАТЬ',
+            style: kButtonTextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RoundIconButton extends StatelessWidget {
+  const RoundIconButton(
+      {super.key, required this.icon, required this.onPressed});
+
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      fillColor: kDarkBlueAccentColor,
+      elevation: 8.0,
+      constraints: const BoxConstraints.tightFor(width: 55.0, height: 55.0),
+      onPressed: onPressed,
+      child: Icon(
+        icon,
+        color: Colors.white,
+      ),
+    );
+  }
+}
